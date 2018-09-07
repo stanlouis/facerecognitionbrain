e5fdc9d03cbf6aeeb66a3091dbb835f22614be9a
Stanley Louis <stanlouis@msn.com>
2018-09-07 08:08:18 -0400

    Add initial components

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Logo from './components/Logo/Logo';
+import Particles from 'react-particles-js';
+import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles params={particlesOptions} className="particles" />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+      </div>
+    );
+  }
+}
+
+export default App;
