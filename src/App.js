51fdf36f8ebcaf0a2a887b277f4746c2bf532df7
Stanley Louis <stanlouis@msn.com>
2018-09-07 10:28:12 -0400

    add clarifai face-detection API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import Logo from './components/Logo/Logo';
 import Particles from 'react-particles-js';
 import './App.css';
+
+const API_KEY = `${process.env.REACT_APP_API_KEY_CLARIFAI}`;
+// initialize with your api key.
+const app = new Clarifai.App({
+  apiKey: API_KEY
+});
 
 const particlesOptions = {
   particles: {
@@ -17,18 +25,32 @@ const particlesOptions = {
     }
   }
 };
-const API_KEY = `${process.env.REACT_APP_API_KEY_CLARIFAI}`;
 
 class App extends Component {
   state = {
-    input: ''
+    input: '',
+    imageUrl: ''
   };
 
   handleInputChange = e => {
-    console.log(e.target.value);
+    this.setState({
+      input: e.target.value
+    });
   };
   handleButtonSubmit = () => {
-    console.log('click');
+    this.setState(() => {
+      return { imageUrl: this.state.input };
+    });
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function(response) {
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bounding_box
+        );
+      },
+      function(err) {
+        // there was an error
+      }
+    );
   };
   render() {
     return (
@@ -41,6 +63,7 @@ class App extends Component {
           onInputChange={this.handleInputChange}
           onButtonSubmit={this.handleButtonSubmit}
         />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
