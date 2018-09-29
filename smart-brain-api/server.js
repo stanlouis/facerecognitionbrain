b8d872c0713422c56bb48196cb591ca708e94c37
Stanley Louis <stanlouis@msn.com>
2018-09-28 20:41:11 -0400

    Clean up and refactor

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -3,6 +3,11 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 const app = express();
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const getProfile = require('./controllers/profile');
+const updateEntryCount = require('./controllers/updateRank');
+
 const port = process.env.PORT || 3000;
 
 const db = knex({
@@ -23,88 +28,17 @@ db.select('*')
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.json(database.users);
-});
-
-app.post('/signin', (req, res) => {
-  db.select('email', 'hash')
-    .from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return db
-          .select('*')
-          .from('users')
-          .where('email', '=', req.body.email)
-          .then(user => {
-            console.log(user);
-            res.json(user[0]);
-          })
-          .catch(err => res.status(400).json('unable to the user'));
-      } else {
-        res.status(400).json('wrong credential');
-      }
-    })
-    .catch(err => res.status(400).json('unable to get user'));
-});
-
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  db.transaction(trx => {
-    trx
-      .insert({
-        hash: hash,
-        email: email
-      })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            name: name,
-            email: loginEmail[0],
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch(err => res.status(400).json('Unable to register'));
-});
-
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  let found = false;
-  db.select('*')
-    .from('users')
-    .where({
-      id: id
-    })
-    .then(user => {
-      if (user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json('Not found');
-      }
-    })
-    .catch(err => res.status(400).json('error getting user'));
-});
-
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users')
-    .where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => res.json(entries[0]))
-    .catch(err => res.status(400).json('unable to get entires'));
-});
+app.get('/', (req, res) => res.json(database.users));
+app.post('/signin', (req, res) => signin.handleSignin(req, res, db, bcrypt));
+app.post('/register', (req, res) =>
+  register.handleRegister(req, res, db, bcrypt)
+);
+app.get('/profile/:id', (req, res) =>
+  getProfile.handleGetProfile(req, res, db)
+);
+app.put('/image', (req, res) =>
+  updateEntryCount.handleEntryCount(req, res, db)
+);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
--- src/App.js
+++ src/App.js
@@ -100,7 +100,7 @@ class App extends Component {
             .then(count => {
               this.setState({
                 user: Object.assign(this.state.user, { entries: count })
-              });
+              }).catch(console.log);
             });
         }
         this.displayFaceBox(this.calculateFaceLocation(response));
